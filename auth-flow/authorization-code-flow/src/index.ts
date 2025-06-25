75d11d6f3fce71574cc808fbf75ec100d06df119
ElCardoso <gr78787@gmail.com>
2025-06-25 10:36:48 -0300

    feat: Adicionando implemtação do nonce

--- auth-flow/authorization-code-flow/src/index.ts
+++ auth-flow/authorization-code-flow/src/index.ts
@@ -1,20 +1,38 @@
 import express from "express";
-
+import crypto from "crypto";
+import session from "express-session";
+import jwt from "jsonwebtoken";
 const app = express();
 
+app.use(
+  session({
+    secret: "your-secret-key", // Chave apenas para modo de desenvolvimento/testes
+    resave: false,
+    saveUninitialized: true,
+  })
+);
+
 app.get("/login", (req, res) => {
-  const loginUrlParams = new URLSearchParams({
-    response_type: "code",
-    client_id: "fullcycle-client",
-    redirect_uri: "http://localhost:3000/callback",
-    scope: "openid",
+  const nonce = crypto.randomBytes(16).toString("base64");
+
+  //@ts-ignore
+  req.session.nonce = nonce;
+  req.session.save(() => {
+    const loginUrlParams = new URLSearchParams({
+      response_type: "code",
+      client_id: "fullcycle-client",
+      redirect_uri: "http://localhost:3000/callback",
+      scope: "openid",
+      nonce,
+    });
+
+    const url = `http://localhost:8080/realms/fullcycle-realm/protocol/openid-connect/auth?${loginUrlParams.toString()}`;
+
+    res.redirect(url);
   });
-
-  const url = `http://localhost:8080/realms/fullcycle-realm/protocol/openid-connect/auth?${loginUrlParams.toString()}`;
-
-  res.redirect(url);
 });
 
+//@ts-expect-error - type mismatch
 app.get("/callback", async (req, res) => {
   const bodyParams = new URLSearchParams({
     grant_type: "authorization_code",
@@ -35,6 +53,28 @@ app.get("/callback", async (req, res) => {
 
   const result = await response.json();
 
+  const payloadAccessToken = jwt.decode(result.access_token) as any;
+  const payloadRefreshToken = jwt.decode(result.refresh_token) as any;
+  const payloadIdToken = jwt.decode(result.id_token) as any;
+
+  if (
+    //@ts-expect-error - type mismatch
+    payloadAccessToken.nonce !== req.session.nonce ||
+    //@ts-expect-error - type mismatch
+    payloadIdToken.nonce !== req.session.nonce ||
+    //@ts-expect-error - type mismatch
+    payloadRefreshToken.nonce !== req.session.nonce
+  ) {
+    return res.status(401).json({ error: "Unauthenticated" });
+  }
+
+  //@ts-expect-error - type mismatch
+  req.session.user = payloadAccessToken;
+  //@ts-expect-error - type mismatch
+  req.session.access_token = result.access_token;
+  //@ts-expect-error - type mismatch
+  req.session.id_token = result.id_token;
+  req.session.save();
   res.json(result);
 });
 
